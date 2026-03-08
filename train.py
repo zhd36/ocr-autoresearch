a4bfa5afe7951d711924466a9b2e7f54e2370ee6
zhd36 <u202215271@hust.edu.cn>
2026-03-08 19:54:34 +0800

    Experiment: total batch 32

--- train.py
+++ train.py
@@ -194,7 +194,7 @@ def get_lr(progress):
 # ---------------------------------------------------------------------------
 
 # Optimization
-TOTAL_BATCH_SIZE = 64
+TOTAL_BATCH_SIZE = 32
 DEVICE_BATCH_SIZE = 32
 EVAL_BATCH_SIZE = 128
 LR = 6e-4
