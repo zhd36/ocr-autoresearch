a3f35e9b5a65255e8df159316772d8827cbf7c50
zhd36 <u202215271@hust.edu.cn>
2026-03-09 00:54:37 +0800

    Experiment: residual pre-RNN adapter

--- train.py
+++ train.py
@@ -105,6 +105,11 @@ class ResNetAsterEncoder(nn.Module):
         self.layer4 = self._make_layer(256, 6, [2, 1])
         self.layer5 = self._make_layer(512, 3, [2, 1])
         self.pre_rnn_norm = nn.LayerNorm(512)
+        self.pre_rnn_adapter = nn.Sequential(
+            nn.Linear(512, 384),
+            nn.GELU(),
+            nn.Linear(384, 512),
+        )
 
         self.rnn = nn.LSTM(
             input_size=512,
@@ -145,6 +150,7 @@ class ResNetAsterEncoder(nn.Module):
         x = self.layer5(x)
         x = x.squeeze(2).transpose(1, 2).contiguous()
         x = self.pre_rnn_norm(x)
+        x = x + self.pre_rnn_adapter(x)
         x, _ = self.rnn(x)
         return x
 
