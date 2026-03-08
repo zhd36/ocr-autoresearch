4c7d19a00a167d020d385ac861eb83ebec4adbd9
zhd36 <u202215271@hust.edu.cn>
2026-03-09 03:43:49 +0800

    Revert "Experiment: add pre-RNN positional bias | OCR_AR_LR=4.5e-4 OCR_AR_BETAS=0.85,0.998 OCR_AR_USE_PRE_RNN_POSITIONAL=true"
    This reverts commit 96a597fda9ce6aa163184dc4e03e382206d36ea7.

--- train.py
+++ train.py
@@ -91,14 +91,7 @@ class ResNetAsterEncoder(nn.Module):
     Adapted from OpenOCR's ResNet_ASTER encoder.
     """
 
-    def __init__(
-        self,
-        in_channels=3,
-        lstm_hidden=256,
-        lstm_layers=2,
-        pre_rnn_mix_kernel=0,
-        use_pre_rnn_positional=False,
-    ):
+    def __init__(self, in_channels=3, lstm_hidden=256, lstm_layers=2, pre_rnn_mix_kernel=0):
         super().__init__()
         self.layer0 = nn.Sequential(
             nn.Conv2d(in_channels, 32, kernel_size=3, stride=1, padding=1, bias=False),
@@ -112,10 +105,6 @@ class ResNetAsterEncoder(nn.Module):
         self.layer3 = self._make_layer(128, 6, [2, 1])
         self.layer4 = self._make_layer(256, 6, [2, 1])
         self.layer5 = self._make_layer(512, 3, [2, 1])
-        self.pre_rnn_positional = None
-        if use_pre_rnn_positional:
-            self.pre_rnn_positional = nn.Parameter(torch.zeros(1, 32, 512))
-            nn.init.normal_(self.pre_rnn_positional, std=0.02)
         self.pre_rnn_mix = None
         if pre_rnn_mix_kernel > 1:
             padding = pre_rnn_mix_kernel // 2
@@ -164,8 +153,6 @@ class ResNetAsterEncoder(nn.Module):
         x = self.layer4(x)
         x = self.layer5(x)
         x = x.squeeze(2).transpose(1, 2).contiguous()
-        if self.pre_rnn_positional is not None:
-            x = x + self.pre_rnn_positional[:, : x.size(1)]
         if self.pre_rnn_mix is not None:
             mixed = self.pre_rnn_mix(x.transpose(1, 2)).transpose(1, 2).contiguous()
             x = x + mixed
@@ -180,7 +167,6 @@ class CRNNConfig:
     lstm_hidden: int = 256
     lstm_layers: int = 2
     pre_rnn_mix_kernel: int = 0
-    use_pre_rnn_positional: bool = False
     dropout: float = 0.1
 
 
@@ -193,7 +179,6 @@ class CRNN(nn.Module):
             lstm_hidden=config.lstm_hidden,
             lstm_layers=config.lstm_layers,
             pre_rnn_mix_kernel=config.pre_rnn_mix_kernel,
-            use_pre_rnn_positional=config.use_pre_rnn_positional,
         )
         self.sequence_norm = nn.LayerNorm(self.encoder.out_channels)
         self.dropout = nn.Dropout(config.dropout)
@@ -301,7 +286,6 @@ EMA_DECAY = env_float("EMA_DECAY", 0.0)
 LSTM_HIDDEN = env_int("LSTM_HIDDEN", 256)
 LSTM_LAYERS = env_int("LSTM_LAYERS", 2)
 PRE_RNN_MIX_KERNEL = env_int("PRE_RNN_MIX_KERNEL", 0)
-USE_PRE_RNN_POSITIONAL = env_bool("USE_PRE_RNN_POSITIONAL", False)
 DROPOUT = env_float("DROPOUT", 0.1)
 
 # Misc
@@ -333,7 +317,6 @@ config = CRNNConfig(
     lstm_hidden=LSTM_HIDDEN,
     lstm_layers=LSTM_LAYERS,
     pre_rnn_mix_kernel=PRE_RNN_MIX_KERNEL,
-    use_pre_rnn_positional=USE_PRE_RNN_POSITIONAL,
     dropout=DROPOUT,
 )
 
